ee3947f0e8e28817a408e515d61590622d44a888
Havel <crepamer1@outlook.com>
2023-02-09 15:07:22 -0300

    fase1

---
+++ start.cs
@@ -0,0 +1,71 @@
+using System;
+
+        // conversion explicita
+        // conversion implicita
+        // cosnt e imput
+
+        // parametros, argumentos y return
+        // ambito
+        // sobre carga y parametros opcionales
+
+        // condicionales if, else if y else + && y ||
+        // swich
+
+        // buble while
+
+namespace sintax{
+    class Program{
+        static void Main(string[] arg) {
+
+
+
+
+
+        }
+
+        static void explicita() {
+            float flo = 23.4F;
+            int num = flo;
+            Console.WriteLine("num");
+        }
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
