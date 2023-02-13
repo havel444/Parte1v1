f8618dfedec1073e911d64821fd9f3c54f700a3b
Havel <crepamer1@outlook.com>
2023-02-13 15:40:10 -0300

    vsCodePhase11

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace cshard{
         static void Main(string[] arg) {
 
             var ki1 = new sintaxASD();
-            ki1.explicita();
+            //ki1.explicita();
             //implicita();
             //constAndImput();
             //methodsASD();
@@ -26,8 +26,12 @@ namespace cshard{
             var ki2 = new methodsASD();
             //Console.WriteLine(ki2.parameters1("Alan", "Torres"));
             //ki2.ambit1();
+            //Console.WriteLine(ki2.sobre1(4, 3.3));
+            //Console.WriteLine(ki2.option1(1, 1, 1));
 
-
+            var ki3 = new conditionalsASD();
+            //ki3.if1();
+            ki3.switch1();
 
 
 
--- methods.cs
+++ methods.cs
@@ -11,9 +11,25 @@ namespace cshard{
             string surname1 = "Zapata";
 
             Console.WriteLine($"Your name is {id1 + " " + name1 + " " + surname1}");
-
         } 
-        
+        public int sobre1(int num1, int num2) => num1 + num2;
+        public double sobre1(int num1, double num2) => num1 - num2;
+        public int option1(int num1, int num2, int num3, int num4 = 100) => num1 + num2 + num3 + num4;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
 
         public int id1 = 222;    
